7f2fa18eb0c47a4693a546e9f0adfd37c6f5d7ed
link <t18370240624@163.com>
2024-10-27 21:25:02 +0800

    articleDetail

---
+++ src/components/ArticleDetail.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const articleTitle = '';
+    const insertTime: Date | null = null;
+    const updateTime: Date | null = null;
+    const clickCount = ref(0);
+</script>
+
+<template>
+    <div class="header">
+        <div class="header-content">
+            <h1>{{ articleTitle }}</h1>
+            <p>发表于{{ insertTime }}|更新于{{ updateTime }}</p>
+            <p>阅读量：{{ clickCount }}</p>
+        </div>    
+    </div>
+
+    <div class="content-info">
+        <div class="wrap">
+            <div class="content">
+                
+            </div>
+        </div>
+    </div>
+
+    
+</template>
